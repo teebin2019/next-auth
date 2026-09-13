86b0ba9bc61b5aeaeb37d9bea4cf3e9dfca9ed9f
teebin2019 <teebin2019@gmail.com>
2026-09-13 12:38:45 +0700

    update products

---
+++ app/users/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Sidebar from "../components/sidebar";
+function UserPage() {
+  return (
+    <>
+      <Sidebar />
+      {/* Main content */}
+      <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+          {/* your existing dashboard content */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UserPage;
