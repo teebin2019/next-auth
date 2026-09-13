d76aebd5e5f4696b8a23acd29530fded1e31c4ab
teebin2019 <teebin2019@gmail.com>
2026-09-13 16:39:42 +0700

    update products

---
+++ app/products/add/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import Sidebar from "@/app/components/sidebar";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+function ProductPage() {
+  const [name, setName] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/products", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: name,
+        }),
+      });
+
+      const result = await res.json();
+
+      alert(result.message);
+      router.push("/products");
+      router.refresh();
+    } catch (err) {
+      console.error(err);
+      alert("เกิดข้อผิดพลาด");
+    }
+  };
+
+  return (
+    <>
+      <Sidebar />
+      {/* Main content */}
+      <div className="p-4 sm:ml-64">
+        <div className="p-4  rounded-lg dark:border-gray-700 mb-3">
+          <div className="flex items-center justify-between">
+            <h2 className="font-bold text-2xl my-2">Add Products</h2>
+          </div>
+          <div className="p-4    rounded-lg dark:border-gray-700">
+            <form className="max-w-sm" onSubmit={handleSubmit}>
+              {/* ชื่อ */}
+              <div className="mb-5">
+                <label
+                  htmlFor="first_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  ชื่อ
+                </label>
+
+                <input
+                  type="text"
+                  id="first_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="John"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+              >
+                บันทึก
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductPage;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,18 +1,116 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Sidebar from "../components/sidebar";
-function DashboardPage() {
+import Link from "next/link";
+
+interface Product {
+  product_id: number;
+  product_name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+function ProductPage() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const fetchData = async () => {
+    const res = await fetch("/api/products");
+    const data = await res.json();
+    setProducts(data?.products);
+  };
+
+  const handleDelete = async (id: number) => {
+    try {
+      const res = await fetch("/api/products/" + id, {
+        method: "DELETE",
+      });
+      const result = await res.json();
+
+      console.log(result);
+
+      alert("ลบข้อมูลสำเร็จ");
+      fetchData();
+    } catch (err) {
+      console.log(err);
+      alert("เกิดข้อผิดพลาด");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <Sidebar />
       {/* Main content */}
       <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-          {/* your existing dashboard content */}
+        <div className="p-4  rounded-lg dark:border-gray-700 mb-3">
+          <div className="flex items-center justify-between">
+            <h2 className="font-bold text-2xl my-2">Products</h2>
+
+            <Link
+              href="/products/add"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              เพิ่ม
+            </Link>
+          </div>
+          <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  {[
+                    "ลำดับ",
+                    "ชื่อ",
+                    "วันที่สร้าง",
+                    "วันที่แก้ไข",
+                    "ดำเนินการ",
+                  ].map((item, idx) => (
+                    <th scope="col" className="px-6 py-3" key={idx}>
+                      {item}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((item, idx) => (
+                  <tr
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+                    key={item.product_id}
+                  >
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {idx + 1}
+                    </th>
+                    <td className="px-6 py-4">{item.product_name}</td>
+                    <td className="px-6 py-4">{item.created_at}</td>
+                    <td className="px-6 py-4">{item.updated_at ?? "-"}</td>
+                    <td className="px-6 py-4 text-left ">
+                      <Link
+                        href={"/products/" + item.product_id + "/edit"}
+                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-2"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        className="font-medium text-red-600 dark:text-red-500 hover:underline mx-2"
+                        onClick={() => handleDelete(item.product_id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
   );
 }
 
-export default DashboardPage;
+export default ProductPage;
